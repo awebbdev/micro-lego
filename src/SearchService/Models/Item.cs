df4fec1996d1c8b59157ab83afd3bc1b714db9e6
Adam R Webb <adamwebb13@gmail.com>
2024-04-30 16:32:11 -0500

    Added Search Service and Temporary Sync Services

---
+++ src/SearchService/Models/Item.cs
@@ -0,0 +1,23 @@
+using MongoDB.Entities;
+
+namespace SearchService
+{
+    public class Item : Entity
+    {
+        public int ReservePrice { get; set; }
+        public string Seller { get; set; }
+        public string Winner { get; set; }
+        public int SoldAmount { get; set; }    
+        public int CurrentHighBid { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime AuctionEnd { get; set; }
+        public string Status { get; set; }
+        public string Name { get; set; }
+        public string Theme { get; set; }
+        public int Year { get; set; }
+        public string SetNumber { get; set; }
+        public int PieceCount { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
